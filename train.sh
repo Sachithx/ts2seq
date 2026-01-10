f9d114427f6bd39338e4a02fd04182ecb6704d4e
Sachithx <e240203@e.ntu.edu.sg>
2026-01-10 15:37:58 +0800

    train sh sbatch

--- train.sh
+++ train.sh
@@ -1,5 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=pix2seq_har
+#SBATCH --gpus=l40:4
+#SBATCH --time=48:00:00
+#SBATCH --output=/projects/pix2seqdata/outputs/train-%j.log
+#SBATCH --error=/projects/pix2seqdata/outputs/train-%j.log
+
+# Load CUDA and cuDNN FIRST (before conda)
+module load CUDA/12.1.1
+module load cuDNN/8.9.2.26-CUDA-12.1.1
+
+# Then load Miniconda
+module load Miniconda3
+source activate
+conda activate test
+
+export TMPDIR=/projects/pix2seqdata/tmp
+cd ~/pix2seq
+
 python3 run.py --mode=train \
-  --model_dir=/projects/pix2seqdata/models/ts_model \
+  --model_dir=/tmp/ts_model \
   --config=configs/config_det_finetune.py \
   --config.dataset.train_file_pattern='/tmp/ts_coco/tfrecords/train*.tfrecord' \
   --config.dataset.val_file_pattern='/tmp/ts_coco/tfrecords/val*.tfrecord' \
